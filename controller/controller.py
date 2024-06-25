d35e69470cdde877c09eae0d55362ab4feb598a6
briz <yussp6969@google.com>
2024-06-25 17:18:48 +0800

    update: add the option of press esc

--- controller/controller.py
+++ controller/controller.py
@@ -42,6 +42,9 @@ class Controller:
 
             if event_pg.type == pg.KEYDOWN:
                 # For orientating
+                if event_pg.key == pg.K_ESCAPE:
+                    print("Press ESC")
+                    # ev_manager.post(EventQuit())
                 key_down_events.append(event_pg)
 
             if event_pg.type == pg.MOUSEBUTTONDOWN:
