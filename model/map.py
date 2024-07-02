73959abddb34a91b7f34b0c0a31ba20050c5a413
LEbullient <b12902067@csie.ntu.edu.tw>
2024-07-02 15:41:28 +0800

    feat(map): add some methods in map

--- model/map.py
+++ model/map.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import os
+import random
 
 import pygame as pg
 
@@ -40,10 +41,41 @@ class Map:
             int(position[1] * self.size[1] / const.ARENA_SIZE[1]), 0, self.size[1] - 1
         )
         return x, y
+    
+    def convert_cell(self, position: tuple | pg.Vector2) -> pg.Vector2:
+        """
+        Given an entity at a certain position in the map, return which cell it's in
+        """
+        x = util.clamp(
+            position[0] * const.ARENA_SIZE[0] / self.size[0], 0, const.ARENA_SIZE[0] - 1
+        )
+        y = util.clamp(
+            position[1] * const.ARENA_SIZE[1] / self.size[1], 0, const.ARENA_SIZE[1] - 1
+        )
+        return pg.Vector2(x, y)
 
     def get_type(self, position: tuple | pg.Vector2) -> int:
+        """
+        Return the type of terrain at a certain position in the map
+        value 1 denotes road, 2 denotes puddle, and 3 denotes obstacle
+        """
         x, y = self.convert_coordinate(position)
         return self.map[x][y]
+    
+    def get_random_pos(self, r: int) -> pg.Vector2:
+        """
+        Return a random position in map that is not of type "obstacle"
+        """
+        ret = pg.Vector2(
+            random.randint(r, const.ARENA_SIZE[0] - r),
+            random.randint(r, const.ARENA_SIZE[1] - r),
+        )
+        while self.get_type(ret) == const.MAP_OBSTACLE:
+            ret = pg.Vector2(
+                random.randint(r, const.ARENA_SIZE[0] - r),
+                random.randint(r, const.ARENA_SIZE[1] - r),
+            )
+        return ret
 
 
 def load_map(map_dir):
