2d3885fb2ac05fbcb98e4bcf51a23dc458f80b4f
shimeming <shimeming@gmail.com>
2024-07-05 00:53:57 +0800

    style(pause_menu): modify the framework of pause_menu
    Let PauseMenu inherit ObjectBase.
    Add many type hints.

--- view/object/pause_menu.py
+++ view/object/pause_menu.py
@@ -4,25 +4,26 @@ from typing import TYPE_CHECKING
 
 import pygame as pg
 
-from const import WINDOW_SIZE
+from view.object.object_base import ObjectBase
 
 if TYPE_CHECKING:
     from model import PauseMenu
 
 
-class PauseMenuView:
-    def __init__(self, pause_menu: PauseMenu, canvas: pg.Surface):
+class PauseMenuView(ObjectBase):
+    def __init__(self, canvas: pg.Surface, ratio: float, pause_menu: PauseMenu):
+        self.image_initialized = True
+        super().__init__(canvas, ratio)
         self.pause_menu = pause_menu
-        self.canvas = canvas
-        self.title_font = pg.font.Font('./font/Cubic_11_1.300_R.ttf', 48)
-        self.font = pg.font.Font('./font/Cubic_11_1.300_R.ttf', 36)
+        self.title_font = pg.font.Font('./font/Cubic_11_1.300_R.ttf', 20*ratio)
+        self.font = pg.font.Font('./font/Cubic_11_1.300_R.ttf', 12*ratio)
         self.options = self.pause_menu.options
 
     def draw(self):
         if not self.pause_menu.enabled:
             return
 
-        bg_surf = pg.Surface((WINDOW_SIZE[0], WINDOW_SIZE[1]), pg.SRCALPHA)
+        bg_surf = pg.Surface(self.canvas.get_rect().size, pg.SRCALPHA)
         bg_surf.fill((200, 200, 200, 100))
 
         center = (bg_surf.get_width() // 2, bg_surf.get_height() // 2)
@@ -35,7 +36,7 @@ class PauseMenuView:
         self.canvas.blit(bg_surf, (0, 0))
 
 
-def draw_text(surf, x, y, text, color, font, underline=False):
+def draw_text(surf: pg.Surface, x: float, y: float, text: str, color, font: pg.Font, underline: bool = False):
     underline_color = 'lightskyblue'
     underline_thickness = 3
     underline_offset = 3
