927e10f4fdc1bddc6a3a56e13954023fe996cedf
Yao <changmingyao2014@gmail.com>
2024-07-04 15:22:25 +0800

    refactor: change the bullet class

--- model/bullet/bullet.py
+++ model/bullet/bullet.py
@@ -8,7 +8,7 @@ import const
 import const.map
 import util
 import view
-from event_manager import EventBulletEnd, EventBulletMove, EventBulletStart
+from event_manager import EventBulletDamage, EventBulletEnd, EventBulletMove, EventBulletStart
 from instances_manager import get_event_manager, get_model
 from model.entity import Entity
 
@@ -16,24 +16,38 @@ if TYPE_CHECKING:
     from model.team import Team
 
 
-class Bullet(Entity):
-    def __init__(self, position: pg.Vector2 | tuple[float, float], target: Entity,
-                 entity_type: str = 'bullet', imgstate: str = 'default',
-                 speed: float = 0.0, is_ranged: bool = False, radius: float = None) -> None:
+class BaseBullet(Entity):
+    def __init__(self, position: pg.Vector2 | tuple[float, float], entity_type: str = 'bullet',
+                 speed: float = 0.0, imgstate: str = 'default') -> None:
         super().__init__(position, entity_type=entity_type, imgstate=imgstate)
         self.direction: pg.Vector2 | tuple[float, float] = None
-        self.target: Entity = target
         self.speed: float = speed
-        self.is_ranged: bool = is_ranged
-        self.radius: float = radius
         self.exist: bool = True
 
-    def move(self, victim: Entity):
+
+class BulletSniper:
+    def __init__(self, position: pg.Vector2 | tuple[float, float], entity_type: str = 'bullet',
+                 speed: float = 0.0, imgstate: str = 'default', victim: Entity = None) -> None:
+        super().__init__(position=position, entity_type=entity_type, imgstate=imgstate, speed=speed)
+        self.victim = victim
+
+    # def move(self):
         """
         Move the bullet in the given direction.
         """
-        original_pos = self.position
-        target_pos = victim.position
-        self.direction = (target_pos - original_pos).normalize()
+        # original_pos = self.position
+        # target_pos = self.target.position
+        # self.direction = (target_pos - original_pos).normalize()
 
-        get_event_manager().post(EventBulletMove(bullet=self, original_pos=original_pos, victim=victim))
+        # if (target_pos - original_pos).length() <= self.speed:
+        #    get_event_manager().post(EventBulletDamage(bullet=self, original_pos=original_pos, victim=victim))
+        # else:
+        #    get_event_manager().post(EventBulletMove(bullet=self, original_pos=original_pos, victim=victim))
+
+
+class BulletRanger:
+    def __init__(self, position: pg.Vector2 | tuple[float, float], entity_type: str = 'bullet',
+                 speed: float = 0.0, imgstate: str = 'default', target: Entity = None, range: float = 0.0) -> None:
+        super().__init__(position=position, entity_type=entity_type, imgstate=imgstate, speed=speed)
+        self.target = target
+        self.range = range
