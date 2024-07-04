8293041660cd8d87b74a86a4e140e707993235cf
yungyaoc <yycheng910@gmail.com>
2024-07-04 17:14:34 +0800

    feat(view/tower_cd): make the cd of tower on the lower-right corner

--- view/object/tower_cd.py
+++ view/object/tower_cd.py
@@ -4,6 +4,7 @@ import time
 from typing import TYPE_CHECKING
 
 import pygame as pg
+from math import pi
 
 import const
 
@@ -20,9 +21,7 @@ class TowerCDView:
         if entity.hidden or entity.spawn_timer is None:
             return
 
-        self.cd_width = (entity.spawn_timer.interval - entity.spawn_timer.get_remaining_time()
-                         ) / entity.spawn_timer.interval * const.ENTITY_RADIUS * 2
-        pg.draw.rect(screen, (0, 0, 0),
-                     (self.entity.position.x - const.ENTITY_RADIUS, self.entity.position.y - const.ENTITY_RADIUS - const.CD_BAR_UPPER, const.ENTITY_RADIUS * 2, 2))
-        pg.draw.rect(screen, (0, 0, 255),
-                     (self.entity.position.x - const.ENTITY_RADIUS, self.entity.position.y - const.ENTITY_RADIUS - const.CD_BAR_UPPER, self.cd_width, 2))
+        radius = const.ENTITY_RADIUS / 1.5
+        pg.draw.circle(screen, 'black', entity.position + pg.Vector2(const.ENTITY_RADIUS, const.ENTITY_RADIUS), radius, width=3)
+        cd_remaining = (entity.spawn_timer.interval - entity.spawn_timer.get_remaining_time()) / entity.spawn_timer.interval
+        pg.draw.arc(screen, const.CD_BAR_COLOR, pg.Rect(entity.position + pg.Vector2(const.ENTITY_RADIUS - radius, const.ENTITY_RADIUS - radius), pg.Vector2(radius*2, radius*2)), pi / 2, pi / 2- pi * 2 * cd_remaining, width=3)
