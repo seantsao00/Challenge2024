f5cd8b06df02af3ac0e4384092c739a7b763f4a2
MiriamLin <miriamlin2005@gmail.com>
2024-07-04 16:40:42 +0800

    docs(Party-Selection): Add docstrings and minor fixes for PartySelection class

--- model/party.py
+++ model/party.py
@@ -1,18 +1,32 @@
 import pygame as pg
 import sys
 import const
+
 class PartySelection:
+    """
+    Class for selecting parties in the game.
+    Each PartySelection has the following properties:
+     - parties: List of available parties to choose from.
+     - selected_indices: List of indices indicating the current selection for each player.
+     - selected_parties: List of parties currently selected by each player.
+     - player_controls: List of control mappings for each player.
+    """
+    
     def __init__(self):
         self.parties = [const.PartyTypes.JUNIOR, const.PartyTypes.FBI, const.PartyTypes.POLICE, const.PartyTypes.BLACK, const.PartyTypes.MAORI]
         self.selected_indices = [0, 0, 0, 0]  
         self.selected_parties = [const.PartyTypes.JUNIOR, const.PartyTypes.JUNIOR, const.const.PartyTypes.JUNIOR, const.PartyTypes.JUNIOR]  
         self.player_controls = [const.PARTY_KEYS_MAP['team1'], const.PARTY_KEYS_MAP['team2'], const.PARTY_KEYS_MAP['team3'], const.PARTY_KEYS_MAP['team4']]
+    
     def handle_select_party(self):
+        """
+        Handle input events for party selection.
+        
+        This function processes QUIT and KEYDOWN events. For KEYDOWN events,
+        it updates the selected party for each player based on their controls.
+        """
         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
+            if event.type == pg.KEYDOWN:
                 for i, controls in enumerate(self.player_controls):
                     if event.key == controls['left']:
                         self.selected_indices[i] = (self.selected_indices[i] + 4) % len(self.parties)
