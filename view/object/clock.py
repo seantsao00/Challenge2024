d6209ff2f1deade26f2ae0431582476882c6a490
PixelCat31415 <jimmychiu31415@gmail.com>
2024-07-13 02:27:00 +0800

    feat (view, clock): changed clock color to fit in the new background

--- view/object/clock.py
+++ view/object/clock.py
@@ -19,7 +19,8 @@ class ClockView(ObjectBase):
         time_remaining = int(const.GAME_TIME - get_model().get_time())
         (min, sec) = divmod(time_remaining, 60)
         font = font_loader.get_font(size=20)
-        time_remaining_surface = font.render(f'{min:02d}:{sec:02d}', True, 'black')
+        time_remaining_surface = font.render(
+            f'{min:02d}:{sec:02d}', False, pg.Color(201, 158, 131))
         self.__canvas.blit(time_remaining_surface, time_remaining_surface.get_rect(
             center=ScreenInfo.translate((50, 30))))
 
