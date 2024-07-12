3489564db032ce08995b17b7688278c859b9a4a6
PixelCat <b12902043@csie.ntu.edu.tw>
2024-07-12 09:21:40 +0800

    feat (scorebox): sort scoreboard by current score

--- view/object/scoreboxes.py
+++ view/object/scoreboxes.py
@@ -17,26 +17,31 @@ class Scorebox:
         self.__team = team
         self.__team_score: int
         self.__team_name_surface = font_loader.get_font(
-            size=16).render(str(team.team_name), False, 'black')
+            size=scorebox.SCOREBOX_FONT_SIZE).render(f"Team {self.__team.team_id}", False, 'black')
         self.__team_avatar = components.createTeamAvatar(
             self.__team, int(ScreenInfo.scale(scorebox.SCOREBOX_AVATAR_SIZE)))
         self.update()
 
     def draw(self, position: tuple[int, int]):
+        pg.draw.rect(self.__canvas, pg.color.Color(255, 255, 255, 127),
+                     (*ScreenInfo.translate((0, 0), position), ScreenInfo.scale(70), ScreenInfo.scale(40)))
         self.__canvas.blit(
             self.__team_name_surface,
-            self.__team_name_surface.get_rect(midleft=ScreenInfo.translate((27, 12.5), position))
+            self.__team_name_surface.get_rect(midleft=ScreenInfo.translate((20, 10), position))
         )
-        score_surface = font_loader.get_font(size=16).render(
+        score_surface = font_loader.get_font(size=scorebox.SCOREBOX_FONT_SIZE).render(
             f"{self.__team_score:.2f}", False, 'black')
         self.__canvas.blit(
             score_surface,
-            score_surface.get_rect(center=ScreenInfo.translate((35, 40), position))
+            score_surface.get_rect(topright=ScreenInfo.translate((70, 18), position))
         )
-        self.__canvas.blit(self.__team_avatar, ScreenInfo.translate((0, 0), position))
+        self.__canvas.blit(self.__team_avatar, ScreenInfo.translate((2, 2), position))
 
     def update(self):
         self.__team_score = self.__team.points
+
+    def cur_points(self):
+        return self.__team_score
 
 
 class ScoreboxesView(ObjectBase):
@@ -53,8 +58,11 @@ class ScoreboxesView(ObjectBase):
         if not self.__initialized:
             self.initialize()
             self.__initialized = True
-        for i, box in enumerate(self.__boxes):
-            box.draw(scorebox.SCOREBOX_ANCHORS[i])
+        self.__boxes.sort(key=lambda box: box.cur_points(), reverse=True)
+        cur_y = scorebox.SCOREBOX_POSITION[1]
+        for box in self.__boxes:
+            box.draw((scorebox.SCOREBOX_POSITION[0], cur_y))
+            cur_y += scorebox.SCOREBOX_HEIGHT + scorebox.SCOREBOX_SPACING
 
     def update(self):
         for box in self.__boxes:
