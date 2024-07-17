42e15c8a75d8068ed52f4c81e18c334e81fbf5c4
shimeming <shimeming@gmail.com>
2024-07-17 23:13:53 +0800

    feat(tool): add tool to add transparent part for png images

---
+++ tool/extend_png.py
@@ -0,0 +1,61 @@
+import argparse
+import os
+
+import cv2
+import numpy as np
+
+
+def extend_transparent(img_path, direction, extension_size):
+    assert os.path.exists(img_path), f'{img_path} does not exist'
+
+    loaded_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    if loaded_image.dtype == np.uint16:
+        loaded_image = (loaded_image / 256).astype(np.uint8)
+    h, w, _ = loaded_image.shape
+    print(f'origin size: ({w}, {h})')
+
+    if direction == 'up':
+        new_h, new_w = h + extension_size, w
+    elif direction == 'down':
+        new_h, new_w = h + extension_size, w
+    elif direction == 'left':
+        new_h, new_w = h, w + extension_size
+    elif direction == 'right':
+        new_h, new_w = h, w + extension_size
+    else:
+        raise ValueError("Direction must be 'up', 'down', 'left', or 'right'")
+
+    # Create a new image with the new dimensions, filled with transparent pixels
+    new_image = np.zeros((new_h, new_w, 4), dtype=np.uint8)
+
+    # Place the original image in the new image based on the direction
+    if direction == 'up':
+        new_image[extension_size:, :] = loaded_image
+    elif direction == 'down':
+        new_image[:h, :] = loaded_image
+    elif direction == 'left':
+        new_image[:, extension_size:] = loaded_image
+    elif direction == 'right':
+        new_image[:, :w] = loaded_image
+
+    return new_image
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='extend_png.py')
+    parser.add_argument('img_path', help='The path of the image.')
+    parser.add_argument('direction',
+                        help='The direction you want to extend. Must be one of "up", "down", "left", "right"')
+    parser.add_argument('extension_size',
+                        help='The length you want to extend. Must be a int. A negative number will result in trimming the image',
+                        type=int)
+    parser.add_argument('-o', '--output',
+                        help='The name of output the file.',
+                        default='extended_image.png')
+    args = parser.parse_args()
+
+    img_path, direction, extension_size = args.img_path, args.direction, args.extension_size
+    extended_image = extend_transparent(img_path, direction, extension_size)
+
+    # Save the result
+    cv2.imwrite(args.output, extended_image)
