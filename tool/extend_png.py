4f17472712e0c6a9a9496651c94d5d6778b1d25f
shimeming <shimeming@gmail.com>
2024-07-17 23:40:26 +0800

    feat(tool/extend_png): let the program take multiple images and regix

--- tool/extend_png.py
+++ tool/extend_png.py
@@ -1,4 +1,5 @@
 import argparse
+import glob
 import os
 
 import cv2
@@ -7,19 +8,20 @@ import numpy as np
 
 def extend_transparent(img_path, direction, extension_size):
     assert os.path.exists(img_path), f'{img_path} does not exist'
+    print(f'image: {img_path}')
 
     loaded_image = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
     if loaded_image.dtype == np.uint16:
         loaded_image = (loaded_image / 256).astype(np.uint8)
     h, w, _ = loaded_image.shape
-    print(f'original size: ({w}, {h})')
+    print(f'original size: ({w}, {h})', end='\t')
 
     if direction in ['up', 'down']:
         new_h, new_w = h + extension_size, w
     elif direction in ['left', 'right']:
         new_h, new_w = h, w + extension_size
     else:
-        raise ValueError("Direction must be 'up', 'down', 'left', or 'right'")
+        raise ValueError('Direction must be "up", "down", "left", or "right"')
 
     # Create a new image with the new dimensions, filled with transparent pixels
 
@@ -51,19 +53,26 @@ def extend_transparent(img_path, direction, extension_size):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='extend_png.py')
-    parser.add_argument('img_path', help='The path of the image.')
+    parser.add_argument('images', help='The path of the images. Can be regex.'
+                        'If multiple images are given, the result image will replace the original images.', nargs='+')
     parser.add_argument('direction',
                         help='The direction you want to extend. Must be one of "up", "down", "left", "right"')
     parser.add_argument('extension_size',
                         help='The length you want to extend. Must be a int. A negative number will result in trimming the image',
                         type=int)
     parser.add_argument('-o', '--output',
-                        help='The name of output the file.',
-                        default='extended_image.png')
+                        help='The name of output the file. Can only be specified when the number of images is one.',
+                        default=None)
     args = parser.parse_args()
-
-    img_path, direction, extension_size = args.img_path, args.direction, args.extension_size
-    extended_image = extend_transparent(img_path, direction, extension_size)
-
-    # Save the result
-    cv2.imwrite(args.output, extended_image)
+    images, direction, extension_size = args.images, args.direction, args.extension_size
+    output_name = args.output
+    images = [file for pattern in images for file in glob.glob(pattern)]
+    if len(images) > 1 and output_name is not None:
+        raise ValueError('You should not specify output name when extending multiple images.')
+    print(f'input images: {images}')
+    for img in images:
+        extended_images = extend_transparent(img, direction, extension_size)
+        if output_name is None:
+            cv2.imwrite('extended_image.png', extended_images)
+        else:
+            cv2.imwrite(output_name, extended_images)
