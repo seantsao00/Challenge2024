b31272f3b7b7bb1db96600090ac55b10df5382bc
Achi-kyw <sereter19@gmail.com>
2024-07-20 12:22:14 +0800

    update(pick): update no banned

--- tool/pick.py
+++ tool/pick.py
@@ -31,19 +31,22 @@ def main(reset):
         for key in data_dict:
             data_dict[key] = 0
     else:
-        print("這一場要 ban 哪些圖呢（請輸入對應的編號以空白隔開）")
+        print("這一場要 ban 哪些圖呢（請輸入對應的編號以空白隔開，輸入0代表不需要）")
         map_name = {1: "破房了，段蓉刪掉", 2: "煉獄的沙朗牛排", 3: "爛台大，台大什麼了不起", 4: "九橋問題",
                     5: "台灣街道", 6: "喔就沙漠，呀咧呀咧", 7: "這是一張無向圖", 8: "乙種建築用地", 9: "臺南"}
         for k, v in map_name.items():
             print(f"{k}: {v}")
         banned = list(map(int, input().split()))
-        cnt = len(banned)
-        weight = []
-        for i in range(1, 10):
-            if i in banned:
-                weight.append(0)
-            else:
-                weight.append(1+(3-data_dict[map_name[i]]))
+        if banned == [0]:
+            weight = [1] * 9
+        else:
+            cnt = len(banned)
+            weight = []
+            for i in range(1, 10):
+                if i in banned:
+                    weight.append(0)
+                else:
+                    weight.append(1+(3-data_dict[map_name[i]]))
         result = random.choices(list(map_name.values()), weights=weight)[0]
         print("後臺精密計算中...")
         time.sleep(0.5)
