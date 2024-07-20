bca19f7060f70b3c81d02bd0d95ed4f2baefad32
Achi-kyw <sereter19@gmail.com>
2024-07-20 12:39:52 +0800

    update(special pick): complete special pick

--- tool/pick.py
+++ tool/pick.py
@@ -31,13 +31,18 @@ def main(reset):
         for key in data_dict:
             data_dict[key] = 0
     else:
+        print("這是第幾場比賽")
+        num = int(input())
         print("這一場要 ban 哪些圖呢（請輸入對應的編號以空白隔開，輸入0代表不需要）")
         map_name = {1: "破房了，段蓉刪掉", 2: "煉獄的沙朗牛排", 3: "爛台大，台大什麼了不起", 4: "九橋問題",
                     5: "台灣街道", 6: "喔就沙漠，呀咧呀咧", 7: "這是一張無向圖", 8: "乙種建築用地", 9: "臺南"}
         for k, v in map_name.items():
             print(f"{k}: {v}")
+        chosen_dict = read_json("tool/special_picked")
         banned = list(map(int, input().split()))
-        if banned == [0]:
+        if str(num) in chosen_dict and num not in banned:
+            result = chosen_dict[str(num)]
+        elif banned == [0]:
             weight = [1] * 9
         else:
             cnt = len(banned)
@@ -62,7 +67,7 @@ def main(reset):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='JSON dictionary manager')
-    parser.add_argument('-r', '--reset', action='store_true', help='reset')
+    parser.add_argument('-r', '--reset', action='store_true', help='reset', )
     args = parser.parse_args()
 
     main(args.reset)
